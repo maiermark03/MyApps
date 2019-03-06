f6d21c869016d1a6287e2d4ae8a270ed03bcf70e
SSZC Szondi György <sszcszondi@hotmail.com>
2019-03-06 11:16:03 +0100

    #2: játék ciklikulása mogoldása

---
+++ kpo/kpo/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///
+            /// Program: Kő papír olló játék leprogramozása
+            /// Írta: maier márk.
+            /// Adott: számítógép  véletlenszerűen választ k/p/o
+            /// Játékos billentyűzeten választ
+
+
+            ///Megoldandó feledat 1:
+            ///Ciklus programfutás amíg a felhasználó szeretne játszani.
+            bool moreGame = true;
+
+            Console.WriteLine("*****Kő/Papír/Olló*****");
+
+            do
+            {
+
+
+
+                Console.Write("Akarsz még játszani? i/n");
+                if (Console.ReadKey(true).KeyChar == 'n')
+                {
+                    moreGame = false;
+                }
+            } while (moreGame);
+
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
