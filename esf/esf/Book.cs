dac3a9d14cd042e4cc7c277e73f9e311998d7e92
SSZC Szondi György <sszcszondi@hotmail.com>
2019-03-20 12:15:21 +0100

    #4: Tipussos lista

---
+++ esf/esf/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace esf
+{
+    internal class Book
+    {
+        public string Name { get; internal set; }
+        public string Author { get; internal set; }
+        public int Page { get; internal set; }
+    }
+}
--- esf/esf/Program.cs
+++ esf/esf/Program.cs
@@ -15,10 +15,10 @@ namespace esf
             //A létrhozot új tipussal inicilizáljunk egy objektumot.
             Excercise ex = new Excercise("Maier Márk");
 
-            //ex.Name = "Maier Márk";
+            //ex.Name = "Maier Márk";.
 
             Console.WriteLine(ex.Name);
-            //tagfügvény
+            //tagfügvény.
             ex.printName();
 
             Console.WriteLine(ex.getName());
@@ -41,7 +41,7 @@ namespace esf
                 v--;
             } while (v > 0);
 
-            //elágazások
+            //elágazások.
             v = 2;
             if (v == 2 )
             {
@@ -72,21 +72,50 @@ namespace esf
             s = Console.ReadLine();
             Console.WriteLine("Ez egy szöveg:");
 
-            //Egy billentyű leütése
-            //Console.ReadKey(true).KeyChar
-            //integerré konvertálni a console számot(string)
-            //int.Parse(Console.WriteLine())
+            //Egy billentyű leütése.
+            //Console.ReadKey(true).KeyChar.
+            //integerré konvertálni a console számot(string).
+            //int.Parse(Console.WriteLine()).
 
-            //Tömbök
+            //Tömbök.
             //Létrehoztunk egy int tipusútömbötaminek a neve szamok.
             int[] szamok = new int[5];
             string[] szoveg = new string[10];
 
-            //Tömb inicializálás
+            //Tömb inicializálás.
             int[] iNums = new int[] { 1, 2, 3, 4, 5, 6 };
             string[] sFruit = new string[] {"Alma","Körte","Eper","Banán"};
             Console.WriteLine("iNums első eleme: {0} sFruit első eleme: {1}", iNums[0], sFruit[0]);
             Console.WriteLine("iNums első eleme:"+ iNums[0] + "sFruit első eleme:" +sFruit[0]);
+
+            //Lista elem
+            var Lista = new List<Book>();
+
+            //Új elemek hozzáadása a listához.
+            //Ctrl-'.'Hívjuk segítségül.
+            Lista.Add(new Book() { Name = "Oroszlánkölykök", Author = "Irwin Shan", Page = 100 });
+            Lista.Add(new Book() { Name = "Megkergetve", Author = "Margó István", Page = 200 });
+            Lista.Add(new Book() { Name = "Egri csillagok", Author = "Gárdonyi Géza", Page = 1200 });
+
+            //Lista kiirása
+            foreach (Book book in Lista)
+            {
+                Console.WriteLine(book.Name + " " + book.Author + " " + book.Page);
+            }
+            //Lista egy elemének törlése
+            Lista.Remove(Lista.Find(x => x.Name.Contains("Oroszlánkölykök")));
+
+            Console.WriteLine("******************************************************************");
+
+            //Lista kiirása
+            foreach (Book book in Lista)
+            {
+                Console.WriteLine(book.Name + " " + book.Author + " " + book.Page);
+            }
+
+           
+            //Lista.Find(x => x.Name.Contains("Oroszlánkölykök"))
+            //Console.WriteLine("\nFind: Könyv címe tartalmaz \"Oroszlánkölykök\": {0}",Lista.Find(x => x.Name.Contains("Oroszlánkölykök")).Name);
 
             Console.ReadKey();
 
