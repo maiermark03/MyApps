4a5c5ae21a996fc09c5b69a78abb098a4af3e790
SSZC Szondi György <sszcszondi@hotmail.com>
2019-04-10 11:47:00 +0200

    #7:tömb

--- tipusconverzio/tipusconverzio/Program.cs
+++ tipusconverzio/tipusconverzio/Program.cs
@@ -10,6 +10,7 @@ namespace tipusconverzio
     {
         static void Main(string[] args)
         {
+
             object a = "123";
             object b = "Hello";
             object c = 10;
@@ -24,10 +25,25 @@ namespace tipusconverzio
 
             int x = 10;
             Console.Write(x == 5 ? "x == 5" : "x != 5");
-
+            // int cast
             for (char ch = 'a'; ch <= 'z'; ch++)
             {
                 Console.Write(ch + "->" +(int)ch+ ' ');
+            }
+            Console.WriteLine();
+            //Tömb
+            int[] iTomb = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < iTomb.Length; i++)
+            {
+                //iTomb[i] = r.Next();
+                Console.Write("Kérek az {0}. számot:", i);
+                iTomb[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            for (int i = 0; i < iTomb.Length; i++)
+            {
+                Console.Write(iTomb[i] + " ");
             }
             Console.ReadKey();
         }
