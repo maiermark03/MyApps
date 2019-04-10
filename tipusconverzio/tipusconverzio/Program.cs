4c1f5e92a4d6455ce0d63e7198a1c64c132662d6
SSZC Szondi György <sszcszondi@hotmail.com>
2019-04-10 11:13:03 +0200

    #7:as

--- tipusconverzio/tipusconverzio/Program.cs
+++ tipusconverzio/tipusconverzio/Program.cs
@@ -10,11 +10,20 @@ namespace tipusconverzio
     {
         static void Main(string[] args)
         {
+            object a = "123";
+            object b = "Hello";
+            object c = 10;
+            string aa = a as string;
+            // expr1 ? v1 : v2;
+            
+            string bb = b as string;
+            Console.WriteLine(bb == null ? "NULL" : bb); // Hello
+
+            string cc = c as string;
+            Console.WriteLine(cc == null ? "NULL" : cc); // Null
+
             int x = 10;
-            if (x is int) //ha x egy int
-            {
-                Console.WriteLine("x típusa int");
-            }
+            Console.WriteLine(x == 5 ? "x == 5" : "x != 5");
 
             Console.ReadKey();
         }
