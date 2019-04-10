ef8f6e991581910d1ab7e050c7e7fbbe17090291
SSZC Szondi György <sszcszondi@hotmail.com>
2019-04-10 12:17:42 +0200

    #7: matrix

--- tipusconverzio/tipusconverzio/Program.cs
+++ tipusconverzio/tipusconverzio/Program.cs
@@ -11,6 +11,7 @@ namespace tipusconverzio
         static void Main(string[] args)
         {
 
+
             object a = "123";
             object b = "Hello";
             object c = 10;
@@ -32,19 +33,50 @@ namespace tipusconverzio
             }
             Console.WriteLine();
             //Tömb
+            int iPrev;
             int[] iTomb = new int[10];
             Random r = new Random();
             for (int i = 0; i < iTomb.Length; i++)
             {
                 //iTomb[i] = r.Next();
                 Console.Write("Kérek az {0}. számot:", i);
-                iTomb[i] = int.Parse(Console.ReadLine());
+                try
+                {
+                    iTomb[i] = int.Parse(Console.ReadLine());
+                    iPrev = i;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Hiba!!nem megfelelő az adat.");
+                    i = iPrev;
+                }
             }
             Console.WriteLine();
+
             for (int i = 0; i < iTomb.Length; i++)
             {
                 Console.Write(iTomb[i] + " ");
             }
+
+            int[] iArr = new int[] { 1, 2, 3, 4, 5 };
+            string[] sArr = new string[] { "alma", "korte" };
+
+            Array.Sort(sArr);
+            for (int i = 0; i < sArr.Length; i++)
+            {
+                Console.Write(sArr[i] + " ");
+
+            }
+
+
+            int[,] matrix = new int[,]
+            {
+                {12, 23, 2 },
+                {13, 67, 52 },
+                {45, 55, 1 }
+            };
+
+
             Console.ReadKey();
         }
     }
